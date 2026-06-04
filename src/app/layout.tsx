67ba36b8ac6fc9801f692457481623b0c3a7b846
sessions-hub <sawada.tm@gmail.com>
2026-06-04 18:26:11 +0900

    feat: Google Analytics追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,14 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H13EV2CLXB" />
+        <script dangerouslySetInnerHTML={{ __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-H13EV2CLXB');
+`}} />
       </head>
       <body>{children}</body>
     </html>
